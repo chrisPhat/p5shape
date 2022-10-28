46f29bd0b4becfbf958979517833ba23b4d08515
Chris Booth <31346759+chrisPhat@users.noreply.github.com>
2022-10-28 12:27:38 +0100

    Add files via upload
    ADDED COMMENTS AND SHAPES

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,16 @@
+//the setup function runs once at the start
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(600, 600);
     canvas.parent("p5container");
 }
 
+//the draw function runs 60 times per second in a continuous loop
 function draw() {
-    background(220);
+    background(220); //greyscale colour for background
 
     fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    ellipse(300, 300, 400);
+
+    fill(0, 0, 0);
+    square(150, 150, 300);
 }
